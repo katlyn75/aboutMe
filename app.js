6a8d136d27bdcc0001ed9fe16b9b0572752b081d
Katlyn Tucker <studioquirk@yahoo.com>
2017-10-26 16:11:29 -0700

    added questions to guessing game

--- app.js
+++ app.js
@@ -1,4 +1,58 @@
   
+'use strict';
+
+let totalScore = 0;
+const userName = prompt('Hi! What\'s your name?');{
+  alert ('Hi, ' + userName + ' want to play a guessing game?');
+}
+
+ //I love sloths.
+ const animal  = prompt ('Do I love sloths?').toLowerCase ();
+  if  (animal === 'yes' || animal=== 'y')
+  {
+    alert ("Sloths are so wierd.  I adore them.");
+  }
+  else (animalResponse === 'no' || 'n');
+  {
+    alert ('There are very few animals I don\'t like.');
+  }
+
+//My least favorite thing is being cold.//
+  const least = prompt ('Do I like the cold?').toLowerCase();
+  if (least === 'Yes' ||  least === 'Y'){
+      alert ('No way, Jose.  I can\'t stand to be cold.');
+  }
+  else if (least === 'No' || least === 'N'){
+    alert ('Now you are really getting to know me!');
+  }
+
+  //Do you thnk I speak any languages besides English?
+  const language = prompt ('Do you think I can speak any languages besides English?').toLowerCase();
+  if (language === 'Yes' || language === 'Y'){
+    alert( 'Well, speak is a stretch, but I know a little French, Spanish and Norwegian');
+  }
+  else if (language != 'Yes' || language != 'Y'){
+      alert ('Speak is a strong word, but I can butcher 3 different languages.');
+  }
+
+  //Do you think I can hula hoop?
+  const hoop = prompt ('Do you think I know how to hula hoop?').toLowerCase();
+  if (hoop === 'Yes' || hoop === 'Y'){
+    alert ('I can hoop and eat a taco at the same time!');
+  }
+  else if (hoop != 'Yes' || hoop != 'Y'){
+    alert ('I can hula hoop for hours.');
+  }
+
+  //How about whistling?
+  const whistle = prompt ('How about whistling?');
+  if (whistle === 'Yes' || whistle === 'Y'){
+    alert ( 'I am a terrible whistler.  One note only.');
+  }
+  else if (whistle != "Yes" || whistle != 'Y'){
+
+    alert ('Good guess!  That skill eludes me.');
+  }
 
   //If I could go anywhere in the world, I would go to New Zealand.
   const array1 = ['iceland', 'new zealand', 'bali'];
@@ -28,23 +82,4 @@
   } 
   console.log ('I sometimes eat ' + foodResponse + ' too.');
 
-
-//I love sloths.
-  const animal  = prompt ('Do I love sloths?').toLowerCase ();
-  if  (animal === 'yes' || animal=== 'y')
-  {
-    alert ("Sloths are so wierd.  I adore them.");
-  }
-  else (animalResponse != sloth);
-  {
-    alert ('There are very few animals I don\'t like.');
-  }
-
-//My least favorite thing is being cold.//
-  const least = prompt ('Do I like the cold?').toLowerCase();
-  if (least === 'Yes' ||  least === 'Y'){
-      alert ('No way, Jose.  I can\'t stand to be cold.');
-  }
-  else if (least === 'No' || least === 'N'){
-    alert ('Now you are really getting to know me!');
-  }
+  alert('Nice job ' + user + ' you got ' + totalScore + ' right!');
