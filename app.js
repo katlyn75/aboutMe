9652d0dbfff7f3a9b6711786ae8bbc6cd8402ae8
Charlie Heiner <heinercj@gmail.com>
2017-10-26 17:12:15 -0700

    put all questions inside functions tested code

--- app.js
+++ app.js
@@ -9,58 +9,64 @@ const userName = prompt('Hi! What\'s your name?');{
 }
 
 questionOne();
+questionTwo();
+questionThree();
+questionFour();
+questionFive();
+questionSix();
+questionSeven();
 
 function questionOne () {
   //I love sloths.
  const animal  = prompt ('Do I love sloths?').toLowerCase ();
  if  (animal === 'yes' || animal=== 'y')
  {
-   alert ("Sloths are so wierd.  I adore them.");
+   alert ("Sloths are so weird.  I adore them.");
+   totalScore++;
  }
  else //(animal === 'no' || animal === 'n');
  {
    alert ('There are very few animals I don\'t like.');
  }
 }
-//  //I love sloths.
-//  const animal  = prompt ('Do I love sloths?').toLowerCase ();
-//   if  (animal === 'yes' || animal=== 'y')
-//   {
-//     alert ("Sloths are so wierd.  I adore them.");
-//   }
-//   else //(animal === 'no' || animal === 'n');
-//   {
-//     alert ('There are very few animals I don\'t like.');
-//   }
 
-
-//My least favorite thing is being cold.//
+function questionTwo () {
+  //My least favorite thing is being cold.//
   const least = prompt ('Do I like the cold?').toLowerCase();
   if (least === 'yes' ||  least === 'y'){
       alert ('No way, Jose.  I can\'t stand to be cold.');
   }
   else if (least === 'no' || least === 'n'){
     alert ('Now you are really getting to know me!');
+    totalScore++;
   }
+}
 
+function questionThree () {
   //Do you thnk I speak any languages besides English?
   const language = prompt ('Do you think I can speak any languages besides English?').toLowerCase();
   if (language === 'yes' || language === 'y'){
     alert( 'Well, speak is a stretch, but I know a little French, Spanish and Norwegian');
+    totalScore++;
   }
   else if (language != 'yes' || language != 'y'){
       alert ('Speak is a strong word, but I can butcher 3 different languages.');
   }
+}
 
+function questionFour () {
   //Do you think I can hula hoop?
   const hoop = prompt ('Do you think I know how to hula hoop?').toLowerCase();
   if (hoop === 'yes' || hoop === 'y'){
     alert ('I can hoop and eat a taco at the same time!');
+    totalScore++
   }
   else if (hoop != 'yes' || hoop != 'y'){
     alert ('I can hula hoop for hours.');
   }
-
+}
+  
+function questionFive () {
   //How about whistling?
   const whistle = prompt ('How about whistling?');
   if (whistle === 'yes' || whistle === 'y'){
@@ -69,34 +75,46 @@ function questionOne () {
   else if (whistle != "yes" || whistle != 'y'){
 
     alert ('Good guess!  That skill eludes me.');
+    totalScore++
   }
+}
 
-  //If I could go anywhere in the world, I would go to New Zealand.
-  const array1 = ['iceland', 'new zealand', 'bali'];
-  const anywhereResponse =  prompt('If I could go anywhere in the world today, where would I go?').toLowerCase();
+function questionSix(){
+   //If I could go anywhere in the world, I would go to New Zealand.
+   const array1 = ['iceland', 'new zealand', 'bali'];
+   const anywhereResponse =  prompt('If I could go anywhere in the world today, where would I go?').toLowerCase();
+ 
+   if (array1.indexOf(anywhereResponse)=== 0)
+     {alert ('Iceland is rumored to be one of the most beautiful places on Earth !');
+      totalScore++;
+    }
+   if (array1.indexOf(anywhereResponse)=== 1){
+     alert ('New Zealand has more sheep than people!');
+     totalScore++;
+   }
+   if (array1.indexOf(anywhereResponse)=== 2)
+     {alert ( 'I would love the beaches of Bali');
+      totalScore++;
+    }
+   else if (array1.indexOf(anywhereResponse != 0,1,2));
+     {alert (anywhereResponse + ' sounds like a beautiful place to visit.');}
+   console.log ('I would like to go to ' + anywhereResponse);
+}
 
-  if (array1.indexOf(anywhereResponse)=== 0)
-    {alert ('Iceland is rumored to be one of the most beautiful places on Earth !');}
-  if (array1.indexOf(anywhereResponse)=== 1){
-    alert ('New Zealand has more sheep than people!');
-  }
-  if (array1.indexOf(anywhereResponse)=== 2)
-    {alert ( 'I would love the beaches of Bali');}
-  else if (array1.indexOf(anywhereResponse != 0,1,2));
-{alert (anywhereResponse + ' sounds like a beautiful place to visit.');}
-  console.log ('I would like to go to ' + anywhereResponse);
-
-//I could eat tacos, pho or chocolate everyday.
+function questionSeven (){
+  //I could eat tacos, pho or chocolate everyday.
   const food = ['tacos', 'pho', 'chocolate']
   const foodResponse = prompt ('What food do I eat more of than you might think possible?').toLowerCase();
   if (food.includes (foodResponse))
   {
     alert ('I never get tired of  ' + foodResponse);
+    totalScore++;
   }
   else
   {
     alert (foodResponse + ' is definitely not right.')
   } 
   console.log ('I sometimes eat ' + foodResponse + ' too.');
+}
 
-  alert('Nice job ' + user + ' you got ' + totalScore + ' right!');
+alert('Nice job ' + userName + ' you got ' + totalScore + ' right!');
